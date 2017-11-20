ebcc1bb0d517d8d883ed4f04f92e78be84675834
Bruno Luiz Santana de Araujo <santanasta@gmail.com>
2017-11-20 15:29:00 -0400

    Email ou telefone opcionais

--- eventex/subscriptions/tests/test_view_new.py
+++ eventex/subscriptions/tests/test_view_new.py
@@ -85,3 +85,11 @@ class SubscriptionsNewPostInvalid(TestCase):
     def test_dont_save_subscription(self):
         """Invalid post must not be saved on model"""
         self.assertFalse(Subscription.objects.exists())
+
+
+class TemplateRegressionTest(TestCase):
+    def test_template_has_non_field_errors(self):
+        invalid_data = dict(name='Bruno Santana]', cpf='12345678901')
+        response = self.client.post(r('subscriptions:new'), invalid_data)
+
+        self.assertContains(response, '<ul class="errorlist nonfield">')
