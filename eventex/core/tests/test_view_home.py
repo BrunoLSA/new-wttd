4f9c582f531912b72db18617b8a957db865fdb23
Bruno Luiz Santana de Araujo <santanasta@gmail.com>
2017-11-27 20:32:21 -0400

    Speaker

--- eventex/core/tests/test_view_home.py
+++ eventex/core/tests/test_view_home.py
@@ -1,8 +1,25 @@
 from django.test import TestCase
 from django.shortcuts import resolve_url as r
 
+from eventex.core.models import Speaker
+
+
 class HomeTest(TestCase):
     def setUp(self):
+        Speaker.objects.create(
+            name='Grace Hopper',
+            slug='grace-hopper',
+            photo='http://hbn.link/hopper-pic',
+            website='http://hbn.link/hopper-site',
+            description='Programadora e almirante.'
+        )
+        Speaker.objects.create(
+            name='Alan Turing',
+            slug='alan-turing',
+            photo='http://hbn.link/turing-pic',
+            website='http://hbn.link/turing-site',
+            description='Programador e matemático.'
+        )
         self.response = self.client.get(r('home'))
 
     def test_get(self):
@@ -20,8 +37,10 @@ class HomeTest(TestCase):
     def test_speakers(self):
         """Must show keynotes speakers"""
         contents = [
+            'href="{}"'.format(r('speaker_detail', slug='grace-hopper')),
             'Grace Hopper',
             'http://hbn.link/hopper-pic',
+            'href="{}"'.format(r('speaker_detail', slug='alan-turing')),
             'Alan Turing',
             'http://hbn.link/turing-pic',
         ]
