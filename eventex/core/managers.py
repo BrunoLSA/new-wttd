e5a142f96298a6f499029287a7835f11596d6337
Bruno Luiz Santana de Araujo <santanasta@gmail.com>
2017-12-06 19:36:29 -0400

    Mixins

--- eventex/core/managers.py
+++ eventex/core/managers.py
@@ -21,10 +21,13 @@ class KindContactManager(models.Manager):
         return self.get_queryset().phones()
 
 
-class PeriodManager(models.Manager):
+class PeriodQuerySet(models.QuerySet):
     MIDDAY = '12:00'
     def at_morning(self):
         return self.filter(start__lt=self.MIDDAY)
 
     def at_afternoon(self):
         return self.filter(start__gte=self.MIDDAY)
+
+
+PeriodManager = models.Manager.from_queryset(PeriodQuerySet)
